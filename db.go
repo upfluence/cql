8be3a951b6f669a2a2573243ebaab4bdfe98eccf
Alexis Montagne <alexis.montagne@gmail.com>
2021-01-06 23:07:11 -0500

    *: Add WithConsistency option

--- db.go
+++ db.go
@@ -14,6 +14,24 @@ type Option interface {
 	IsCQLOption()
 }
 
+type Consistency uint16
+
+const (
+	Any         Consistency = 0x00
+	One         Consistency = 0x01
+	Two         Consistency = 0x02
+	Three       Consistency = 0x03
+	Quorum      Consistency = 0x04
+	All         Consistency = 0x05
+	LocalQuorum Consistency = 0x06
+	EachQuorum  Consistency = 0x07
+	LocalOne    Consistency = 0x0A
+)
+
+type WithConsistency Consistency
+
+func (WithConsistency) IsCQLOption() {}
+
 type NamedQuery string
 
 func (nq NamedQuery) IsCQLOption() {}
